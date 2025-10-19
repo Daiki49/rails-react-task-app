c16bb535f1397b9f98389bab08e8fa14e771a45f
pikattya <daiki.kato.tech@gmail.com>
2025-10-19 16:27:56 +0900

    ホスト許可を修正

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << "rails-react-task-app.onrender.com"
 end
