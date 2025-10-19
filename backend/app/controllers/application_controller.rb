fd63d0951688f9fc25ff3e3d9e011294513a63b5
pikattya <daiki.kato.tech@gmail.com>
2025-10-19 16:44:21 +0900

    CSRF保護に関する変更

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :null_session, if: -> {
+    request.format.json? || request.content_type == "application/json"
+  }
 end
